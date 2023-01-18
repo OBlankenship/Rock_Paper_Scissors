0dea58122bdeed1a8b19b0d1a0fe55cc36234044
Oliver Blankenship <oliverblankenship@gmail.com>
2023-01-18 01:43:17 -0600

    Refactor all functions
    Refactored all functions for simplicity. One event handler now
    handles events for all buttons.

--- rps.js
+++ rps.js
@@ -1,118 +1,70 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-function computerPlay() {
-    let randInt = getRandomInt(3)
-    switch(randInt) {
-        case 0:
-            return "ROCK"
-        case 1:
-            return "PAPER"
-        case 2:
-            return "SCISSORS"
-    }
+function getComputerSelection() {
+    const selections = ["rock", "paper", "scissors"];
+    const randomPosition = Math.floor(Math.random() * selections.length);
+    return selections[randomPosition];
 }
 
-function getInput () {
-    let userInput = ""
-    while (true) {
-        userInput = window.prompt("Please enter rock, paper, or scissors: ");
-        userInput = userInput.toUpperCase();
-        if (userInput === "ROCK" || userInput === "SCISSORS" || userInput === "PAPER") {
-            break
+function playRound(event) {
+    const content = document.getElementById("notification");
+    let playerSelection = event.currentTarget.innerHTML;
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = getComputerSelection();
+    let winner;
+
+    if (playerSelection == computerSelection) {
+        content.textContent = "Tie! Try again!";
+        winner = "tie";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        content.textContent = "Paper beats rock! You win!";
+        winner =  "player";
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        content.textContent = "Scissors beats paper! You win!";
+        winner =  "player";
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        content.textContent = "Rock beats scissors! You win!";
+        winner =  "player";
+    }
+    else {
+        content.textContent = `${computerSelection} beats ${playerSelection}! You lose!`;
+        winner =  "computer";
+    }
+    checkScore(winner);
+    return winner;
+}
+
+let computerScore = 0;
+let playerScore = 0;
+
+function updateScores() {
+    const playerScoreDisplay = document.getElementById("playerScore");
+    playerScoreDisplay.textContent = playerScore;
+    const computerScoreDisplay = document.getElementById("computerScore");
+    computerScoreDisplay.textContent = computerScore;
+}
+
+function checkScore(winner) {
+    switch(winner) {
+        case "computer":
+            computerScore++;;
+            break;
+        case "player":
+            playerScore++;
+            break;
         }
-        window.alert("Invalid input!")
-    }
-    return userInput
-    
-}
-
-function playRound (userInput) {
-    let computerInput = computerPlay();
-    let content = document.getElementById("notification")
-    if (userInput === "PAPER" && computerInput === "ROCK") {
-        content.textContent = "You selected paper, and the computer selected rock!";
-        return "PLAYER"
-    }
-    if (userInput === "PAPER" && computerInput === "SCISSORS") {
-        content.textContent = "You selected paper, and the computer selected scissors!";
-        return "COMPUTER"
-    }
-    if (userInput === "SCISSORS" && computerInput === "PAPER") {
-        content.textContent = "You selected scissors, and the computer selected paper!"
-        return "PLAYER"
-    }
-    if (userInput === "SCISSORS" && computerInput === "ROCK") {
-        content.textContent = "You selected scissors, and the computer selected rock!"
-        return "COMPUTER"
-    }
-    if (userInput === "ROCK" && computerInput === "PAPER") {
-        content.textContent = "You selected rock, and the computer selected paper!"
-        return "COMPUTER"
-    }
-    if (userInput === "ROCK" && computerInput === "SCISSORS") {
-        content.textContent = "You selected rock, and the computer selected scissors!"
-        return "PLAYER"
-    }
-    content.textContent = "Tie! Try again!"
-    return "TIE"
-}
-
-let playerScore = 0
-let computerScore = 0
-
-function keepScore(winner) {
-    if (winner === "PLAYER") {
-        playerWin();
-        
-    }
-    else if (winner === "COMPUTER") {
-        computerWin();
-    }
-
-    if (playerScore === 5) {
-        window.alert("Game Winner: Player")
-    }
-    if (computerScore === 5) {
-        window.alert("Game Winner: Computer")
+    updateScores()
+    if (playerScore === 5 || computerScore === 5) {
+        const winner = playerScore > computerScore ? "Player" : "Computer";
+        const content = document.getElementById("notification");
+        content.textContent = `Game over! ${winner} wins!`;
     }
 }
 
-function computerWin() {
-    computerScore += 1
-    let computerDiv = document.getElementById("computerDiv")
-    let content = document.getElementById("computerScore")
-    content.textContent = `${computerScore}`;
-}
-
-function playerWin() {
-    playerScore += 1
-    let playerDiv = document.getElementById("playerDiv")
-    let content = document.getElementById("playerScore")
-    content.textContent = `${playerScore}`;
-}
-
-function clickRock() {
-    winner = playRound("ROCK")
-    keepScore(winner)
-}
-
-function clickPaper() {
-    winner = playRound("PAPER")
-    keepScore(winner)
-}
-
-function clickScissors() {
-    winner = playRound("SCISSORS")
-    keepScore(winner)
-}
-
-let btnRock = document.getElementById("ROCK")
-btnRock.addEventListener('click', clickRock)
-
-let btnPaper = document.getElementById("PAPER")
-btnPaper.addEventListener('click', clickPaper)
-
-let btnScissors = document.getElementById("SCISSORS")
-btnScissors.addEventListener('click', clickScissors)
+const rockButton = document.getElementById("rockButton");
+rockButton.addEventListener("click", playRound);
+const paperButton = document.getElementById("paperButton");
+paperButton.addEventListener("click", playRound);
+const scissorsButton = document.getElementById("scissorsButton");
+scissorsButton.addEventListener("click", playRound);
